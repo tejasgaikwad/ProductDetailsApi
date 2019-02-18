194ed1768ff63a86e9fe9c714d1fe6013895f287
tejasrgaikwad@gmail.com <tejasrgaikwad@gmail.com>
2019-02-19 00:03:58 +0530

    Improvements

--- src/main/java/com/springboot/productDetails/service/ProductDetailsServiceImpl.java
+++ src/main/java/com/springboot/productDetails/service/ProductDetailsServiceImpl.java
@@ -1,9 +1,11 @@
 package com.springboot.productDetails.service;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -36,25 +38,26 @@ public class ProductDetailsServiceImpl implements ProductDetailsService {
 
 	private Response transformResponse(Response response, String labelType) {
 		Response transformedResponse = new Response();
-		transformedResponse.setProducts(getProcessableProducts(response.getProducts(), labelType));
+		transformedResponse.setProducts(getProcessableProducts(
+				response.getProducts(), labelType));
 		return transformedResponse;
 	}
-	
-	private Products[] getProcessableProducts(Products[] products, String labelType)
-	{
+
+	private Products[] getProcessableProducts(Products[] products,
+			String labelType) {
 		List<Products> productsList = new LinkedList<Products>();
 		for (Products product : products) {
 
 			Price price = product.getPrice();
-			if(price !=null)
-			{
-				if((price.getWas() == null || StringUtils.isEmpty(price.getWas()))
-						|| (price.getNow() == null || StringUtils.isEmpty(price.getWas())))
-				{
+			if (price != null) {
+				if ((price.getWas() == null || StringUtils.isEmpty(price
+						.getWas()))
+						|| (price.getNow() == null || StringUtils.isEmpty(price
+								.getWas()))) {
 					continue;
 				}
 			}
-			
+
 			// populate RGP color code for each basic color.
 			if (product.getColorSwatches() != null) {
 				transformColorSwatches(product.getColorSwatches());
@@ -64,30 +67,31 @@ public class ProductDetailsServiceImpl implements ProductDetailsService {
 			populateNowPrice(product);
 
 			// populate PriceLabel
-			populatePriceLabel(product, (labelType == null? "ShowWasNow": labelType));
-			
+			populatePriceLabel(product, (labelType == null ? "ShowWasNow"
+					: labelType));
+
 			productsList.add(product);
 		}
-		
+
 		// sort by reduction
 		Collections.sort(productsList, Collections.reverseOrder());
-		
-		Products [] productArray = productsList.toArray(new Products[productsList.size()]);
-		
+
+		Products[] productArray = productsList
+				.toArray(new Products[productsList.size()]);
+
 		return productArray;
-		//return productsList;
+		// return productsList;
 	}
 
 	/*
 	 * This method populates RGB color to colorSwatches
 	 */
 	private void transformColorSwatches(ColorSwatches[] colorSwatchesArray) {
-		for (ColorSwatches colorSwatches : colorSwatchesArray) {
-			colorSwatches.setRgbColor((productUtils.getRGB(colorSwatches
-					.getBasicColor())));
-		}
+		Arrays.stream(colorSwatchesArray).forEach(
+				colorSwatches -> colorSwatches.setRgbColor((productUtils
+						.getRGB(colorSwatches.getBasicColor()))));
 	}
-	
+
 	/*
 	 * This function populates the PriceLabel.
 	 */
@@ -95,41 +99,34 @@ public class ProductDetailsServiceImpl implements ProductDetailsService {
 
 		StringBuilder builder = new StringBuilder();
 		switch (labelType) {
-			case "ShowWasThenNow":
-				builder.append("Was ")
-						.append(getFormattedPrice(product.getPrice().getWas()))
-						.append(", then ")
-						.append(((product.getPrice().getThen2() != null && product
-								.getPrice().getThen2().length() != 0) ? getFormattedPrice(product
-								.getPrice().getThen2()) : getFormattedPrice(product
-								.getPrice().getThen1()))).append(", now ")
-						.append(getFormattedPrice(product.getPrice().getNow()));
-				break;
-			case "ShowPercDscount":
-				String was = product.getPrice().getWas();
-				String now = product.getPrice().getNow();
-				Double wasDouble = new Double(was);
-				Double nowDouble = new Double(now);
-				double diff = wasDouble - nowDouble;
-				double percentage =  (diff / wasDouble * 100.00d);
-				builder.append(Math.round(percentage))
-				.append("% off - now ")
-				.append(getFormattedPrice(now));
-				break;
-			case "ShowWasNow":
-				// ShowWasNow
-				builder.append("Was ")
-						.append(getFormattedPrice(product.getPrice().getWas()))
-						.append(", now ")
-						.append(getFormattedPrice(product.getPrice().getNow()));
-				break;
-			default:
-				// ShowWasNow
-				builder.append("Was ")
-						.append(getFormattedPrice(product.getPrice().getWas()))
-						.append(", now ")
-						.append(getFormattedPrice(product.getPrice().getNow()));
-				break;
+		case "ShowWasThenNow":
+			builder.append("Was ")
+					.append(getFormattedPrice(product.getPrice().getWas()))
+					.append(", then ")
+					.append(((product.getPrice().getThen2() != null && product
+							.getPrice().getThen2().length() != 0) ? getFormattedPrice(product
+							.getPrice().getThen2()) : getFormattedPrice(product
+							.getPrice().getThen1()))).append(", now ")
+					.append(getFormattedPrice(product.getPrice().getNow()));
+			break;
+		case "ShowPercDscount":
+			String was = product.getPrice().getWas();
+			String now = product.getPrice().getNow();
+			Double wasDouble = new Double(was);
+			Double nowDouble = new Double(now);
+			double diff = wasDouble - nowDouble;
+			double percentage = (diff / wasDouble * 100.00d);
+			builder.append(Math.round(percentage)).append("% off - now ")
+					.append(getFormattedPrice(now));
+			break;
+		// handles all including ShowWasNow
+		default:
+			// ShowWasNow
+			builder.append("Was ")
+					.append(getFormattedPrice(product.getPrice().getWas()))
+					.append(", now ")
+					.append(getFormattedPrice(product.getPrice().getNow()));
+			break;
 
 		}
 		product.setPriceLabel(builder.toString());
@@ -145,10 +142,8 @@ public class ProductDetailsServiceImpl implements ProductDetailsService {
 		}
 	}
 
-
-	
 	/*
-	 * This method returns formatted price 
+	 * This method returns formatted price
 	 */
 	private String getFormattedPrice(String price) {
 		DecimalFormat format = new DecimalFormat("##.##");
@@ -176,8 +171,8 @@ public class ProductDetailsServiceImpl implements ProductDetailsService {
 				maxFractionDigits = 0;
 			}
 
-			String formattedPrice = ProductDetailsConstants.getNumberFormat("GDP",
-					maxFractionDigits).format(new Double(nowPrice));
+			String formattedPrice = ProductDetailsConstants.getNumberFormat(
+					"GDP", maxFractionDigits).format(new Double(nowPrice));
 			return formattedPrice;
 		}
 		return "";
